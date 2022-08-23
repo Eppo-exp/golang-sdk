c072857f1072415f1b2d3a00bde08df474c40243
Pavel Khrebto <pavel@fluxon.com>
2022-08-23 17:27:04 +0300

    Add EppoClient test for subject with attributes and rules

--- eppoclient/client_test.go
+++ eppoclient/client_test.go
@@ -81,7 +81,7 @@ func Test_LogAssignment(t *testing.T) {
 	mockLogger.AssertNumberOfCalls(t, "LogAssignment", 1)
 }
 
-func Test_GetAssignmentHandlesLoggingException(t *testing.T) {
+func Test_GetAssignmentHandlesLoggingPanic(t *testing.T) {
 	var mockLogger = new(MockLogger)
 	mockLogger.Mock.On("LogAssignment", mock.AnythingOfType("string")).Panic("logging panic")
 
@@ -106,5 +106,46 @@ func Test_GetAssignmentHandlesLoggingException(t *testing.T) {
 	assignment, _ := client.GetAssignment("user-1", "experiment-key-1", Dictionary{})
 	expected := "control"
 
+	assert.Equal(t, expected, assignment)
+}
+
+func Test_AssignSubjectWithAttributesAndRules(t *testing.T) {
+	var mockLogger = new(MockLogger)
+	mockLogger.Mock.On("LogAssignment", mock.AnythingOfType("string")).Return()
+
+	var matchesEmailCondition = Condition{operator: "MATCHES", value: ".*@eppo.com", attribute: "email"}
+	var textRule = Rule{conditions: []Condition{matchesEmailCondition}}
+	var mockConfigRequestor = new(MockConfigRequestor)
+	var overrides = make(Dictionary)
+	var mockVariations = []Variation{
+		{Name: "control", ShardRange: ShardRange{Start: 0, End: 10000}},
+	}
+	var mockResult = ExperimentConfiguration{
+		Name:            "recommendation_algo",
+		PercentExposure: 100,
+		Enabled:         true,
+		SubjectShards:   1000,
+		Overrides:       overrides,
+		Variations:      mockVariations,
+		Rules:           []Rule{textRule},
+	}
+	mockConfigRequestor.Mock.On("GetConfiguration", "experiment-key-1").Return(mockResult, nil)
+
+	client := NewEppoClient(mockConfigRequestor, mockLogger)
+
+	expected := ""
+	assignment, _ := client.GetAssignment("user-1", "experiment-key-1", Dictionary{})
+	assert.Equal(t, expected, assignment)
+
+	assignment, _ = client.GetAssignment("user-1", "experiment-key-1", Dictionary{
+		"email": "test@example.com",
+	})
+	assert.Equal(t, expected, assignment)
+
+	expected = "control"
+
+	assignment, _ = client.GetAssignment("user-1", "experiment-key-1", Dictionary{
+		"email": "test@eppo.com",
+	})
 	assert.Equal(t, expected, assignment)
 }
