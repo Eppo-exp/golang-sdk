83d429e5262048abfa5c98cba78b53f7c37b3947
Pavel Khrebto <pavel@fluxon.com>
2022-08-23 17:57:28 +0300

    Add remaining tests

--- eppoclient/client_test.go
+++ eppoclient/client_test.go
@@ -177,3 +177,47 @@ func Test_WithSubjectInOverrides(t *testing.T) {
 	assignment, _ := client.GetAssignment("user-1", "experiment-key-1", Dictionary{})
 	assert.Equal(t, expected, assignment)
 }
+
+func Test_WithSubjectInOverridesExpDisabled(t *testing.T) {
+	var mockLogger = new(MockLogger)
+	mockLogger.Mock.On("LogAssignment", mock.AnythingOfType("string")).Return()
+
+	var mockConfigRequestor = new(MockConfigRequestor)
+	var mockVariations = []Variation{
+		{Name: "control", ShardRange: ShardRange{Start: 0, End: 100}},
+	}
+	var overrides = make(Dictionary)
+	overrides["d6d7705392bc7af633328bea8c4c6904"] = "override-variation"
+	var mockResult = ExperimentConfiguration{
+		Name:            "recommendation_algo",
+		PercentExposure: 100,
+		Enabled:         false,
+		SubjectShards:   1000,
+		Overrides:       overrides,
+		Variations:      mockVariations,
+		Rules:           []Rule{textRule},
+	}
+
+	mockConfigRequestor.Mock.On("GetConfiguration", "experiment-key-1").Return(mockResult, nil)
+
+	client := NewEppoClient(mockConfigRequestor, mockLogger)
+
+	expected := "override-variation"
+	assignment, _ := client.GetAssignment("user-1", "experiment-key-1", Dictionary{})
+	assert.Equal(t, expected, assignment)
+}
+
+func Test_WithNullExpConfig(t *testing.T) {
+	var mockLogger = new(MockLogger)
+	mockLogger.Mock.On("LogAssignment", mock.AnythingOfType("string")).Return()
+
+	var mockConfigRequestor = new(MockConfigRequestor)
+	mockConfigRequestor.Mock.On("GetConfiguration", "experiment-key-1").Return(ExperimentConfiguration{}, nil)
+
+	client := NewEppoClient(mockConfigRequestor, mockLogger)
+
+	expected := ""
+	assignment, _ := client.GetAssignment("user-1", "experiment-key-1", Dictionary{})
+
+	assert.Equal(t, expected, assignment)
+}
