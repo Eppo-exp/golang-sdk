a7fc14591966c1f7692aeb6b6c0dbfdfce683a13
Pavel Khrebto <pavel@fluxon.com>
2022-08-25 17:54:08 +0300

    Add e2e test

--- eppoclient/eppoclient_e2e_test.go
+++ eppoclient/eppoclient_e2e_test.go
@@ -2,13 +2,20 @@ package eppoclient
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
+	"time"
 
 	"cloud.google.com/go/storage"
+	"github.com/stretchr/testify/assert"
 	"google.golang.org/api/iterator"
 	"google.golang.org/api/option"
 )
@@ -16,8 +23,90 @@ import (
 const TEST_DATA_DIR = "test-data/assignment"
 const BUCKET_NAME = "sdk-test-data"
 
+var testData = []TestData{}
+
 func Test_e2e(t *testing.T) {
+	serverUrl := initFixture()
+
+	client := InitClient(Config{baseUrl: serverUrl, apiKey: "dummy", assignmentLogger: AssignmentLogger{}})
+
+	time.Sleep(2 * time.Second)
+
+	for _, experiment := range testData {
+		expName := experiment.Experiment
+
+		assignments := []string{}
+
+		for _, subject := range experiment.Subjects {
+			assignment, err := client.GetAssignment(subject, expName, Dictionary{})
+
+			if assignment != "" {
+				assert.Nil(t, err)
+			}
+
+			assignments = append(assignments, assignment)
+		}
+
+		assert.Equal(t, experiment.ExpectedAssignments, assignments)
+	}
+}
+
+func initFixture() string {
 	downloadTestData()
+	testResponse := getTestData() // this is here because we need to append to global testData
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch strings.TrimSpace(r.URL.Path) {
+		case "/randomized_assignment/config":
+			json.NewEncoder(w).Encode(testResponse)
+		default:
+			http.NotFoundHandler().ServeHTTP(w, r)
+		}
+	}))
+
+	return server.URL
+}
+
+func getTestData() Dictionary {
+	files, err := ioutil.ReadDir(TEST_DATA_DIR)
+
+	if err != nil {
+		panic("test cases files read error")
+	}
+
+	for _, file := range files {
+		jsonFile, _ := os.Open(TEST_DATA_DIR + "/" + file.Name())
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		defer jsonFile.Close()
+
+		testCaseDict := TestData{}
+		byteValue, _ := ioutil.ReadAll(jsonFile)
+		json.Unmarshal(byteValue, &testCaseDict)
+		testData = append(testData, testCaseDict)
+	}
+
+	expConfigs := Dictionary{}
+
+	for _, experimentTest := range testData {
+		experimentName := experimentTest.Experiment
+		expMap := Dictionary{}
+		expMap["subjectShards"] = 10000
+		expMap["enabled"] = true
+		expMap["variations"] = experimentTest.Variations
+		expMap["name"] = experimentName
+		expMap["percentExposure"] = experimentTest.PercentExposure
+
+		expConfigs[experimentName] = expMap
+	}
+
+	response := Dictionary{}
+	response["experiments"] = expConfigs
+
+	return response
 }
 
 func downloadTestData() {
@@ -26,7 +115,7 @@ func downloadTestData() {
 			log.Fatal(err)
 		}
 	} else {
-		return
+		return //data is already downloaded, skip this step
 	}
 
 	ctx := context.Background()
@@ -47,7 +136,6 @@ func downloadTestData() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(attrs)
 		obj := bkt.Object(attrs.Name)
 		rdr, err := obj.NewReader(ctx)
 
