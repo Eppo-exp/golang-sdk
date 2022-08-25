7bd9c9f74671474b5608bcaebe3f276c1cad5bbe
Pavel Khrebto <pavel@fluxon.com>
2022-08-25 14:41:08 +0300

    Add downloading of test data func

---
+++ eppoclient/eppoclient_e2e_test.go
@@ -0,0 +1,65 @@
+package eppoclient
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"testing"
+
+	"cloud.google.com/go/storage"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
+)
+
+const TEST_DATA_DIR = "test-data/assignment"
+const BUCKET_NAME = "sdk-test-data"
+
+func Test_e2e(t *testing.T) {
+	initTestData()
+}
+
+func initTestData() {
+	if _, err := os.Stat(TEST_DATA_DIR); os.IsNotExist(err) {
+		if err := os.MkdirAll(TEST_DATA_DIR, os.ModePerm); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx, option.WithoutAuthentication())
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	query := &storage.Query{Prefix: "assignment/test-case"}
+	bkt := client.Bucket(BUCKET_NAME)
+	it := bkt.Objects(ctx, query)
+
+	for {
+		attrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(attrs)
+		obj := bkt.Object(attrs.Name)
+		rdr, err := obj.NewReader(ctx)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rdr.Close()
+
+		out, err := os.Create("test-data/" + obj.ObjectName())
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer out.Close()
+
+		io.Copy(out, rdr)
+	}
+}
