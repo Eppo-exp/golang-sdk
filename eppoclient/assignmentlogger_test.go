00d9d9868684242e33b8c18f5bded6f69b1d96fc
Leo Romanovsky <leo@geteppo.com>
2023-12-22 09:05:42 -0800

    Add unit test to verify undesired functionality (FF-1382)

---
+++ eppoclient/assignmentlogger_test.go
@@ -0,0 +1,62 @@
+package eppoclient
+
+import (
+	"encoding/json"
+	"reflect"
+	"testing"
+)
+
+// TestAssignmentEventSerialization tests the JSON serialization and deserialization of AssignmentEvent
+func TestAssignmentEventSerialization(t *testing.T) {
+	// Create a test case with each type
+	testCases := []AssignmentEvent{
+		{
+			Experiment:        "testExperiment",
+			FeatureFlag:       "testFeatureFlag",
+			Allocation:        "testAllocation",
+			Variation:         Bool(true),
+			Subject:           "testSubject",
+			Timestamp:         "testTimestamp",
+			SubjectAttributes: dictionary{"testKey": String("testValue")},
+		},
+		{
+			Experiment:        "testExperiment",
+			FeatureFlag:       "testFeatureFlag",
+			Allocation:        "testAllocation",
+			Variation:         Numeric(123.45),
+			Subject:           "testSubject",
+			Timestamp:         "testTimestamp",
+			SubjectAttributes: dictionary{"testKey": String("testValue")},
+		},
+		{
+			Experiment:        "testExperiment",
+			FeatureFlag:       "testFeatureFlag",
+			Allocation:        "testAllocation",
+			Variation:         String("testVariation"),
+			Subject:           "testSubject",
+			Timestamp:         "testTimestamp",
+			SubjectAttributes: dictionary{"testKey": String("testValue")},
+		},
+		// todo: test JSON variation types.
+	}
+
+	for _, original := range testCases {
+		// Marshal to JSON
+		marshaled, err := json.Marshal(original)
+		if err != nil {
+			t.Errorf("Failed to marshal: %v", err)
+		}
+
+		// Unmarshal from JSON
+		var unmarshaled Value
+		err = json.Unmarshal(marshaled, &unmarshaled)
+		if err != nil {
+			t.Errorf("Failed to unmarshal: %v", err)
+		}
+
+		// Compare the original and unmarshaled
+		if !reflect.DeepEqual(original, unmarshaled) {
+			t.Errorf("Original and unmarshaled Value are not equal. Original: %+v, Unmarshaled: %+v", original, unmarshaled)
+		}
+	}
+}
