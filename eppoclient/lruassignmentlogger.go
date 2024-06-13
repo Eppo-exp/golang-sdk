121cf0ad1d981a961ec73edb12b1d8a64415a312
Oleksii Shmalko <oleksii@fluxon.com>
2024-06-13 18:13:45 +0300

    FF-2331 Implement LRU assignment cache (#42)

---
+++ eppoclient/lruassignmentlogger.go
@@ -0,0 +1,53 @@
+package eppoclient
+
+import (
+	"github.com/hashicorp/golang-lru/v2"
+)
+
+type LruAssignmentLogger struct {
+	cache *lru.TwoQueueCache[cacheKey, cacheValue]
+	inner IAssignmentLogger
+}
+
+// We are only interested in whether a subject was ever a part of an
+// assignment. We are not interested in the order of assignments or
+// knowing the latest assignment. Therefore, both allocation and
+// variation are part of the cacheKey.
+type cacheKey struct {
+	flag       string
+	subject    string
+	allocation string
+	variation  string
+}
+type cacheValue struct {
+}
+
+func NewLruAssignmentLogger(logger IAssignmentLogger, cacheSize int) IAssignmentLogger {
+	cache, err := lru.New2Q[cacheKey, cacheValue](cacheSize)
+	if err != nil {
+		// err is only returned if `cacheSize` is invalid
+		// (e.g., <0) which should normally never happen.
+		panic(err)
+	}
+	return &LruAssignmentLogger{
+		cache: cache,
+		inner: logger,
+	}
+}
+
+func (self *LruAssignmentLogger) LogAssignment(event AssignmentEvent) {
+	key := cacheKey{
+		flag:       event.FeatureFlag,
+		subject:    event.Subject,
+		allocation: event.Allocation,
+		variation:  event.Variation,
+	}
+	value := cacheValue{}
+	previousValue, recentlyLogged := self.cache.Get(key)
+	if !recentlyLogged || previousValue != value {
+		self.inner.LogAssignment(event)
+		// Adding to cache after `LogAssignment` returned in
+		// case it panics.
+		self.cache.Add(key, value)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,14 +2,15 @@ module github.com/Eppo-exp/golang-sdk/v3
 
 go 1.19
 
-require github.com/stretchr/testify v1.8.0
-
 require (
 	github.com/Masterminds/semver/v3 v3.2.1
+	github.com/hashicorp/golang-lru/v2 v2.0.7
+	github.com/stretchr/testify v1.9.0
+)
+
+require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.4.0 // indirect
-	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
