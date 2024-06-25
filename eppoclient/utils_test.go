43d3a5446d31fcb7b12cba603a2e3e363fcd8dd0
Aaron Silverman <aaron.silverman@geteppo.com>
2024-06-24 21:07:03 -0600

    tests passing

--- eppoclient/utils_test.go
+++ eppoclient/utils_test.go
@@ -1,6 +1,7 @@
 package eppoclient
 
 import (
+	"math"
 	"testing"
 )
 
@@ -12,11 +13,21 @@ func TestToFloat64(t *testing.T) {
 		expectErr bool
 	}{
 		{"Float64Input", 123.456, 123.456, false},
-		{"StringInputValid", "789.012", 789.012, false},
+		{"Float32Input", float32(123.456), 123.456, false},
+		{"Int8Input", int8(123), 123.0, false},
+		{"Int16Input", int16(123), 123.0, false},
+		{"Int32Input", int16(123), 123.0, false},
+		{"Int64Input", int16(123), 123.0, false},
+		{"UInt8Input", uint8(123), 123.0, false},
+		{"UInt16Input", uint16(123), 123.0, false},
+		{"UInt32Input", uint16(123), 123.0, false},
+		{"UInt64Input", uint16(123), 123.0, false},
+		{"StringIntInputValid", "789", 789.0, false},
+		{"StringFloatInputValid", "789.012", 789.012, false},
+		{"StringNegativeInputValid", "-789.012", -789.012, false},
 		{"StringInputInvalid", "abc", 0, true},
 		{"SemVerInputInvalid", "1.2.3", 0, true},
 		{"BoolInput", true, 0, true},
-		{"IntInput", 123, 0, true},
 	}
 
 	for _, tt := range tests {
@@ -26,7 +37,8 @@ func TestToFloat64(t *testing.T) {
 				t.Errorf("ToFloat64(%v) error = %v, expectErr %v", tt.input, err, tt.expectErr)
 				return
 			}
-			if !tt.expectErr && result != tt.expected {
+			closeEnough := math.Abs(result - tt.expected) < 0.00001
+			if !tt.expectErr && !closeEnough {
 				t.Errorf("ToFloat64(%v) = %v, want %v", tt.input, result, tt.expected)
 			}
 		})
