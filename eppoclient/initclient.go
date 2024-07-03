282776324b36f27e58423bbdacac439533b71a5a
Leo Romanovsky <leo@geteppo.com>
2024-07-03 16:00:25 -0700

    bump to 4.0.2

--- eppoclient/initclient.go
+++ eppoclient/initclient.go
@@ -4,7 +4,7 @@ package eppoclient
 
 import "net/http"
 
-var __version__ = "4.0.1"
+var __version__ = "4.0.2"
 
 // InitClient is required to start polling of experiments configurations and create
 // an instance of EppoClient, which could be used to get assignments information.
